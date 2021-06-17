59c1e60dbbd0718865dc38214b89e0d3413a7da3
John <johnfiddle7@gmail.com>
2021-06-17 11:24:44 +0200

    v1.5.0.1
    - protocol window translation

--- Log.Designer.cs
+++ Log.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Log));
             this.button1 = new System.Windows.Forms.Button();
             this.listDone = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -36,54 +37,39 @@
             // 
             // button1
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(375, 473);
+            resources.ApplyResources(this.button1, "button1");
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 26);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Schließen";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listDone
             // 
-            this.listDone.Alignment = System.Windows.Forms.ListViewAlignment.Default;
-            this.listDone.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.listDone, "listDone");
             this.listDone.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2});
             this.listDone.GridLines = true;
             this.listDone.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.listDone.HideSelection = false;
-            this.listDone.LabelWrap = false;
-            this.listDone.Location = new System.Drawing.Point(12, 12);
             this.listDone.Name = "listDone";
-            this.listDone.Size = new System.Drawing.Size(463, 455);
-            this.listDone.TabIndex = 1;
             this.listDone.UseCompatibleStateImageBehavior = false;
             this.listDone.View = System.Windows.Forms.View.Details;
             // 
             // columnHeader1
             // 
-            this.columnHeader1.Text = "Quelle";
+            resources.ApplyResources(this.columnHeader1, "columnHeader1");
             // 
             // columnHeader2
             // 
-            this.columnHeader2.Text = "Ziel";
+            resources.ApplyResources(this.columnHeader2, "columnHeader2");
             // 
             // Log
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(487, 511);
             this.Controls.Add(this.listDone);
             this.Controls.Add(this.button1);
             this.Name = "Log";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Protokoll";
             this.Shown += new System.EventHandler(this.Log_Shown);
             this.Resize += new System.EventHandler(this.Log_Resize);
             this.ResumeLayout(false);
